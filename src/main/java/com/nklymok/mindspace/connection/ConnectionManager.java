f5a461442c8a6a65a4c15a0f7fb98f0e216a932d
Nazarii Klymok <37501778+knazariy@users.noreply.github.com>
2020-08-09 01:40:11 +0300

    Deployed

--- src/main/java/com/nklymok/mindspace/connection/ConnectionManager.java
+++ src/main/java/com/nklymok/mindspace/connection/ConnectionManager.java
@@ -5,6 +5,7 @@ import org.apache.log4j.Logger;
 import java.lang.reflect.Method;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class ConnectionManager {
@@ -14,6 +15,13 @@ public class ConnectionManager {
     private static final String DB_URL = "jdbc:h2:~/mindspace";
     private static final String DB_USERNAME = "sa";
     private static final String DB_PASSWORD = "";
+    private static final String DB_CREATE = "CREATE TABLE IF NOT EXISTS tasks(\n" +
+            "    id INT PRIMARY KEY AUTO_INCREMENT,\n" +
+            "    header VARCHAR(80) NOT NULL,\n" +
+            "    description VARCHAR(200),\n" +
+            "    duedate TIMESTAMP,\n" +
+            "    priority INT NOT NULL\n" +
+            ");";
 
     private ConnectionManager() {
 
@@ -27,12 +35,22 @@ public class ConnectionManager {
             try {
                 Class.forName(DB_DRIVER);
                 connection = DriverManager.getConnection(DB_URL, DB_USERNAME,DB_PASSWORD);
+                createTable();
                 return connection;
             } catch (ClassNotFoundException | SQLException e) {
                 logger.error("Failed to connect");
                 e.printStackTrace();
             }
             throw new RuntimeException("Failed to connect");
+        }
+    }
+
+    private static void createTable() {
+        try {
+            PreparedStatement preparedStatement = connection.prepareStatement(DB_CREATE);
+            preparedStatement.execute();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
         }
     }
 
