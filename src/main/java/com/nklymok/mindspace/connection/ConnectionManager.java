8cf6d6910dc7fce2b2a14fbf03ab8f11ea3f2442
Nazarii Klymok <37501778+knazariy@users.noreply.github.com>
2020-09-05 23:48:57 +0300

    SQL moved to .properties

--- src/main/java/com/nklymok/mindspace/connection/ConnectionManager.java
+++ src/main/java/com/nklymok/mindspace/connection/ConnectionManager.java
@@ -1,31 +1,49 @@
 package com.nklymok.mindspace.connection;
 
+import java.io.*;
+import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
     private static Connection connection;
-    private static final String DB_DRIVER = "org.h2.Driver";
-    private static final String DB_URL = "jdbc:h2:~/mindspace";
-    private static final String DB_USERNAME = "sa";
-    private static final String DB_PASSWORD = "";
-    private static final String DB_CREATE = "CREATE TABLE IF NOT EXISTS tasks(\n" +
-            "    id INT PRIMARY KEY AUTO_INCREMENT,\n" +
-            "    header VARCHAR(80) NOT NULL,\n" +
-            "    description VARCHAR(200),\n" +
-            "    duedate TIMESTAMP,\n" +
-            "    priority INT NOT NULL\n" +
-            ");";
+    private static String DB_DRIVER;
+    private static String DB_URL;
+    private static String DB_USERNAME;
+    private static String DB_PASSWORD;
+    private static String DB_CREATE;
 
     private static boolean closed = false;
 
     private ConnectionManager() {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(new File(this.getClass().getResource("/sql.properties").toURI())));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
 
+//        DB_DRIVER = properties.getProperty("sql.driver");
+        DB_DRIVER = "org.h2.Driver";
+        DB_URL = properties.getProperty("sql.url");
+        DB_USERNAME = properties.getProperty("sql.username");
+        DB_PASSWORD = properties.getProperty("sql.password");
+        DB_CREATE = properties.getProperty("sql.createdb");
     }
 
-    public static Connection openConnection() {
+    private static final class Helper {
+        private static final ConnectionManager instance = new ConnectionManager();
+    }
+
+    public static ConnectionManager getInstance() {
+        return Helper.instance;
+    }
+
+    public Connection openConnection() {
         if (connection != null) {
             return connection;
         } else {
@@ -42,7 +60,7 @@ public class ConnectionManager {
         }
     }
 
-    private static void createTable() {
+    private void createTable() {
         try {
             PreparedStatement preparedStatement = connection.prepareStatement(DB_CREATE);
             preparedStatement.execute();
